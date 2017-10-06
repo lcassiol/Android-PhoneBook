1862e91c53877dd933bd5f8f16018f4e4fbbbade
Cassio Carvalho <lccassiol@gmail.com>
2017-10-05 22:11:36 -0300

    Create Activity Form

---
+++ app/src/main/java/com/example/lcassiol/android_phonebook/Form.java
@@ -0,0 +1,21 @@
+package com.example.lcassiol.android_phonebook;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class Form extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+
+}
