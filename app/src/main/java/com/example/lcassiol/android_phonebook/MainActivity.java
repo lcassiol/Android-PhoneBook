d9d3fb752c2bf3b814ad44955bdc8923679641ef
Cassio Carvalho <lccassiol@gmail.com>
2017-10-12 22:52:03 -0300

    Implement insert contact, and delete from screen

--- app/src/main/java/com/example/lcassiol/android_phonebook/MainActivity.java
+++ app/src/main/java/com/example/lcassiol/android_phonebook/MainActivity.java
@@ -1,16 +1,25 @@
 package com.example.lcassiol.android_phonebook;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.ContextMenu;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView contactList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +36,61 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        contactList = (ListView) findViewById(R.id.contactList);
+        registerForContextMenu(contactList);
     }
 
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) menuInfo;
+        final Contact selectContact = (Contact) contactList.getAdapter().getItem(info.position);
+
+        final MenuItem itemCall = menu.add("Ligar");
+        final MenuItem itemSMS = menu.add("SMS");
+        final MenuItem itemSite = menu.add("Site");
+        final MenuItem itemDelete = menu.add("Delete");
+
+        itemDelete.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+
+                new AlertDialog.Builder(MainActivity.this)
+                        .setIcon(android.R.drawable.ic_dialog_alert)
+                        .setTitle("Apagar Contato")
+                        .setMessage("Deseja Realmente Apagar " + selectContact.getName() + " ?")
+                        .setPositiveButton("Sim", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                ContactDAO dao = new ContactDAO(MainActivity.this);
+                                dao.deleteContact(selectContact);
+                                dao.close();
+                                loadList();
+                            }
+                        })
+                        .setNegativeButton("Não", null).show();
+
+
+                return false;
+            }
+        });
+
+        super.onCreateContextMenu(menu, v, menuInfo);
+    }
+
+    @Override
+    protected void onResume() {
+        loadList();
+        super.onResume();
+    }
+
+    private void loadList() {
+        ContactDAO dao = new ContactDAO(this);
+        List<Contact> contacs = dao.getList();
+        dao.close();
+
+        ContactAdapter adapter = new ContactAdapter(this,contacs);
+        this.contactList.setAdapter(adapter);
+    }
 }
